ec31dd620f079d0e4160fe50782ca2fbfd3d3d1e
JoseantoAlro <jalvrom2609@g.educaand.es>
2026-01-14 13:10:59 +0100

    ejercico 4.1 de interfaces y primeros graficos

--- Tema4.1/Ejercicio1.py
+++ Tema4.1/Ejercicio1.py
@@ -1,65 +1,63 @@
 import pandas as pd
 import datapane as dp
 
-fichero_csv = "DI_U05_A02_02.csv"
+fichero_csv = "DI_U05_A02_PP_E_01.csv"
 df = pd.read_csv(fichero_csv)
 
 table = dp.Table(df)
 data_table = dp.DataTable(df)
 
+imagen = dp.Media(file='icono.jpg')
+
 titulo = dp.HTML(
     '<p style="font-size:30px; text-align:center; color:#ffffff; background-color:#4d4d4d;">Informe de ventas</p>'
 )
 
-vendedores_julio = df[(df['Mes'] == 'Julio') & (df['Unidades'] > 500)]
-tabla_julio = dp.DataTable(vendedores_julio)
+ventas_totales = df['Ventas'].sum()
+
+ventas_anuales = df.groupby('Año')['Ventas'].sum().reset_index()
+maximo_ventas = ventas_anuales['Ventas'].idxmax()
+mejor_año = ventas_anuales.loc[maximo_ventas, 'Año']
+valor_mejor_año = ventas_anuales.loc[maximo_ventas, 'Ventas']
 
 
+total_ventas_final = dp.BigNumber(
+    heading='Ventas totales',
+    value= int(ventas_totales),
 
-
-
-filtro_limite = df[df['Unidades'] <= 300] #todos los que sean inferiores a 300
-
-indice_max_importe = filtro_limite['Importe (€)'].idxmax() #el de mayor importe de la anterior seleccion
-
-record_ventas = filtro_limite.loc[indice_max_importe] #todos los datos de esa fila
-
-
-record_ventas = dp.Group(
-    dp.BigNumber(
-        heading=f"Récord: {record_ventas['Nombre']} ({record_ventas['Mes']})",
-        value=f"{record_ventas['Importe (€)']}€",
-        label="Máximo importe con menos de 300 unidades"
-    ),
-    columns=1
 )
 
-
-
-nombres_s = df[(df['Nombre'].str.startswith('S')) & (df['Mes'] == 'Agosto')] #todos los vendedores de agosto que empiezan por s
-
-max_ventas_importe = nombres_s['Importe (€)'].idxmax() #el que mas vendió
-
-max_ventas = nombres_s.loc[max_ventas_importe]
-
-record_ventas_agosto = dp.Group(
-    dp.BigNumber(
-        heading=f"Récord: {max_ventas['Nombre']} ({max_ventas['Mes']})",
-        value=f"{max_ventas['Importe (€)']}€",
-        label="Máximo importe con menos de 300 unidades"
-    ),
-    columns=1
+mejor_año_final =dp.BigNumber(
+    heading='Año con msa ventas',
+    value=int(mejor_año),
 )
 
+texto_descriptivo = dp.Text(
+    "> **Justificación para la directiva:** Estos valores permiten evaluar el crecimiento histórico de la empresa "
+    "además de identificar el año de mayor rendimiento, lo cual es crucial para aprender del mejor momento que se ha experimentado."
+)
+
+db21 = df[df['Año']==2021]
+ventas21 = db21['Ventas'].sum()
+
+db20 = df[df['Año']==2020]
+ventas20 = db20['Ventas'].sum()
 
 
-
+cambio_anual = dp.BigNumber(
+    heading='Ventas en 2021',
+    value=ventas21,
+    change=ventas21 - ventas20,
+    is_upward_change=ventas21 > ventas20
+)
 
 report = dp.Report(
-    titulo,      # Ahora aparecerá el título arriba
-    tabla_julio,
-    record_ventas,
-    record_ventas_agosto,
-    data_table   # La tabla interactiva (opcional, si quieres mostrarla)
+    imagen,
+    titulo,
+    total_ventas_final,
+    mejor_año_final,  
+    texto_descriptivo, 
+    cambio_anual,
+    data_table   
 )
 report.save(path='DI_U05_A02_05.html', open=True)
